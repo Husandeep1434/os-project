46b3187e901403fb19d849362315ee51be81bcd5
Husandeep Kaur <husanbrar1434@gmail.com>
2026-04-15 17:27:30 +0530

    energy efficient algorithm

--- schduling/project.py
+++ schduling/project.py
@@ -54,3 +54,40 @@ def energy_priority(gantt):
     for pid, start, end in gantt:
         energy += (2.0**2) * (end - start)   
     return energy
+def eaas(processes):
+    time = 0
+    ready = []
+    gantt = []
+    processes.sort(key=lambda x: x.arrival)
+
+    while processes or ready:
+        while processes and processes[0].arrival <= time:
+            ready.append(processes.pop(0))
+
+        if ready:
+            ready.sort(key=lambda x: (x.priority, x.burst))
+            p = ready.pop(0)
+
+            start = time
+            end = time + p.burst
+            gantt.append((p.pid, start, end))
+            time = end
+        else:
+            time += 1
+
+    return gantt
+def energy_eaas(gantt):
+    energy = 0
+    for pid, start, end in gantt:
+        burst = end - start
+
+        if burst <= 3:
+            freq = 1.0
+        elif burst <= 6:
+            freq = 1.5
+        else:
+            freq = 2.0
+
+        energy += (freq**2) * burst
+
+    return energy
